379876e4cc9fa87951cf151a1fdf91a432aef1b5
RajoraC <chprashantbadshah.com>
2023-02-09 22:03:42 +0530

    new file adding

---
+++ a2.java
@@ -0,0 +1,19 @@
+class  A
+{
+ 
+  public static void main(String... s)
+
+ {
+  System.out.println("hello java actuly demo");
+
+}
+
+}
+class B
+{
+public static void main(String...   a)
+ {
+  System.out.println(" prashant chaudhary");
+
+}
+}
